1936d56c70fb8641d335ca89d64ff7a717a09255
mahinyayaa <mahinyayaa@gmail.com>
2020-10-15 22:28:41 -0700

    updated

--- src/components/Home/Clients/Client/Client.js
+++ src/components/Home/Clients/Client/Client.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Client = ({client}) => {
     return (
-        <div className="col-lg-2 col-sm-4 mb-5">
+        <div className="col-lg-2 col-md-4 mb-5">
             <div className="d-flex justify-content-center">
                 <img style={{height:'50px'}} src={client.img} alt=""/>
             </div>
